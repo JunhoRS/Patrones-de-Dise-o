c5cea941385bdda9f639d9225e2f85129984e16d
JunhoRS <126283654+JunhoRS@users.noreply.github.com>
2023-07-31 16:19:01 -0400

    added project design patterns

---
+++ Patrones de Diseño/AppConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Patrones_de_Diseño
+{
+    public class AppConfig
+    {
+        // Variables y métodos del Singleton, como se mostró en ejemplos anteriores
+        // ...
+
+        // Propiedades públicas para configuración
+        public string AppName { get; set; }
+        public string Version { get; set; }
+        public string Theme { get; set; }
+
+        private AppConfig()
+        {
+            // Inicializamos algunas configuraciones predeterminadas
+            AppName = "Mi Aplicación";
+            Version = "1.0";
+            Theme = "Predeterminado";
+        }
+
+        private static AppConfig instance;
+
+        public static AppConfig GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new AppConfig();
+            }
+            return instance;
+        }
+    }
+}
---
+++ Patrones de Diseño/Program.cs
@@ -0,0 +1,34 @@
+﻿using Patrones_de_Diseño;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones_de_Diseño
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mostrar un mensaje de bienvenida en la consola
+            Console.WriteLine("Bienvenido al programa de ejemplo de Singleton y Patrones de Diseño.");
+
+            // Obtener la instancia de AppConfig utilizando el patrón Singleton
+            var config = AppConfig.GetInstance();
+
+            // Acceder y modificar las propiedades de configuración
+            Console.WriteLine("Nombre de la aplicación: " + config.AppName);
+            Console.WriteLine("Versión: " + config.Version);
+            Console.WriteLine("Tema actual: " + config.Theme);
+
+            // Cambiar el tema
+            config.Theme = "Oscuro";
+            Console.WriteLine("Tema actualizado a: " + config.Theme);
+
+            // Punto de pausa para evitar que la consola se cierre inmediatamente
+            Console.ReadLine();
+        }
+    }
+}
+
